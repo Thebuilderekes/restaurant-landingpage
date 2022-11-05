c534a80785662a92d5057090b4fd1ae75fc41403
Ekeopre Beredugo <68848865+Thebuilderekes@users.noreply.github.com>
2022-11-05 14:22:04 +0100

    updated file path to css

--- js/style.js
+++ js/style.js
@@ -3,19 +3,17 @@ const hamburger = document.querySelector("#hamburger-icon");
 const closeIcon = document.querySelector("#closemenu-icon");
 const mobileNav = document.querySelector("#mobile-nav");
 const body = document.querySelector("#body");
-
 closeIcon.style.display = "none";
 mobileNav.style.display = "none";
 
 //Hamburger menu click functionality
-hamburger.addEventListener("click", function () {
-  
+hamburger.addEventListener("click", function() {
   console.log("working")
 });
 
 
 //close menu click functionality
-closeIcon.addEventListener("click", function () {
+closeIcon.addEventListener("click", function() {
   mobileNav.style.display = "none";
   hamburger.style.display = "block";
   closeIcon.style.display = "none";
