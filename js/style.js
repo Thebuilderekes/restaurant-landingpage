98ac075238913d44ab68eac3d5ea71813ba3e89c
Ekeopre Beredugo <68848865+Thebuilderekes@users.noreply.github.com>
2022-11-05 13:59:51 +0100

    added navigation and menu section

---
+++ js/style.js
@@ -0,0 +1,26 @@
+//Navigation menu fuctionality
+const hamburger = document.querySelector("#hamburger-icon");
+const closeIcon = document.querySelector("#closemenu-icon");
+const mobileNav = document.querySelector("#mobile-nav");
+const body = document.querySelector("#body");
+
+closeIcon.style.display = "none";
+mobileNav.style.display = "none";
+
+//Hamburger menu click functionality
+hamburger.addEventListener("click", function () {
+  
+  console.log("working")
+});
+
+
+//close menu click functionality
+closeIcon.addEventListener("click", function () {
+  mobileNav.style.display = "none";
+  hamburger.style.display = "block";
+  closeIcon.style.display = "none";
+});
+
+
+
+
