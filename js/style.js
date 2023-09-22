f32b7f51d5b560d47d16a41ed137210ff4746dfd
thebuilderekes <ekeopreberedugo1@gmail.com>
2023-09-22 22:32:25 +0100

    updated

--- js/style.js
+++ js/style.js
@@ -1,4 +1,5 @@
 //Navigation menu fuctionality
+const navItems = document.querySelector("nav ul li");
 const hamburger = document.getElementById("hamburgermenu-icon");
 const closeIcon = document.getElementById("closemenu-icon");
 const mobileNav = document.getElementById("mobile-nav");
@@ -7,18 +8,26 @@ mobileNav.style.display = "none";
 
 //Hamburger menu click functionality
 hamburger.addEventListener("click", function (event) {
-  if (mobileNav.style.display === "none") {
-    mobileNav.style.display = "block";
-    hamburger.style.display = "none";
-    closeIcon.style.display = "block";
-  }
+	if (mobileNav.style.display === "none") {
+		mobileNav.style.display = "block";
+		hamburger.style.display = "none";
+		closeIcon.style.display = "block";
+	}
 
-  console.log("working");
+	console.log("working");
 });
 
 //close menu click functionality
 closeIcon.addEventListener("click", function () {
-  mobileNav.style.display = "none";
-  hamburger.style.display = "block";
-  closeIcon.style.display = "none";
+	mobileNav.style.display = "none";
+	hamburger.style.display = "block";
+	closeIcon.style.display = "none";
+});
+
+window.addEventListener("mouseup", function (event) {
+	if (event.target != navItems) {
+		mobileNav.style.display = "none";
+		hamburger.style.display = "block";
+		closeIcon.style.display = "none";
+	}
 });
